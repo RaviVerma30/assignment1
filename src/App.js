68b5db0c20889d6c54f1e827d75566a9de916d5e
Ravi Prakash Verma <96530102+RaviVerma30@users.noreply.github.com>
2022-02-05 11:53:36 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <div id="root">
+          <p>I am learning React. My life is getting better.</p>
+        </div>
+      </>
+    );
+  }
+}
+export default App;
